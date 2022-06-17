cda94b5c2fb7060051c116dab0d7928182310253
Himanshu Raj <himanshurajverma549@gmail.com>
2022-06-17 16:43:02 +0530

    add dropdown filter upfor client review

--- category-access.js
+++ category-access.js
@@ -1,67 +1,154 @@
 let richText = document.querySelector(".rte.w-richtext");
 let allHeads = [...richText.querySelectorAll("h2, h3, h4, h5, h6")];
 let ddWrapper = document.querySelector("[data-category='wrapper']");
-let ddContainer = ddWrapper? ddWrapper.parentElement : null;
-function addCategories(){
-    if(ddContainer == null)return;
+let ddContainer = ddWrapper ? ddWrapper.parentElement : null;
+
+function addCategories() {
+
+    if (ddContainer == null && allHeads.length > 0) return;
+
     allHeads.forEach(heads => {
-        // *** See note
         let headItem = heads.nextElementSibling;
         let clonedWrapper = ddWrapper.cloneNode(true);
         let hTwoHead = clonedWrapper.querySelector("[data-head='h2']");
         let ddMenu = clonedWrapper.querySelector("[data-list='dropdown']");
         let ddMenuHead = clonedWrapper.querySelector("[data-head='h3']");
         let ddItem = clonedWrapper.querySelector("[data-head='heading']");
-        if(heads.tagName == 'H2'){
-            hTwoHead.innerText = heads.innerHTML;
+        let dropdown = clonedWrapper.querySelector("[data-item='dd']");
+
+        if (heads.tagName == 'H2') {
+
+            hTwoHead.innerHTML = heads.innerText;
+            hTwoHead.setAttribute("data-category-title", convertToSlug(heads.innerText));
+            heads.setAttribute("data-title", convertToSlug(heads.innerText));
             while (headItem && headItem.tagName !== 'H2') {
+
                 if (allHeads.includes(headItem) && headItem.tagName == 'H3') {
                     let firstCategory = ddMenuHead;
-                    firstCategory.innerHTML = headItem.innerHTML;
+                    firstCategory.innerHTML = headItem.innerText;
+                    firstCategory.setAttribute("data-category-title", convertToSlug(headItem.innerText));
+                    headItem.setAttribute("data-title", convertToSlug(headItem.innerText));
+                    console.log("h3")
                 }
-                else if(allHeads.includes(headItem) && headItem.tagName == 'H4'){
+
+                else if (allHeads.includes(headItem) && headItem.tagName == 'H4') {
+
                     let secondCategory = null;
-                    if(ddMenuHead.innerText != "Subcategory"){
-                        secondCategory = ddItem
-                        secondCategory.innerHTML = headItem.innerHTML;
+
+                    if (ddMenuHead.innerHTML != "Subcategory") {
+                        secondCategory = ddItem.cloneNode(true)
                         ddMenu.appendChild(secondCategory)
+                        // console.log("h4")
                     }
-                    else{
+
+                    else {
                         secondCategory = ddMenuHead
-                        secondCategory.innerHTML = headItem.innerHTML;
+                        // console.log("h3 new")
                     }
+                    secondCategory.innerHTML = headItem.innerText;
+                    secondCategory.setAttribute("data-category-title", convertToSlug(headItem.innerText));
+                    headItem.setAttribute("data-title", convertToSlug(headItem.innerText));
+
                 }
-                else if(allHeads.includes(headItem) && headItem.tagName == 'H5'){
+
+                else if (allHeads.includes(headItem) && headItem.tagName == 'H5') {
+
                     let thirdCategory = null;
-                    if(ddMenuHead.innerText != "Subcategory"){
+
+                    if (ddMenuHead.innerHTML != "Subcategory") {
                         thirdCategory = ddItem.cloneNode(true);
-                        thirdCategory.innerHTML = headItem.innerHTML;
                         ddMenu.appendChild(thirdCategory)
+                        // console.log("h5")
                     }
-                    else{
+
+                    else {
                         thirdCategory = ddMenuHead
-                        thirdCategory.innerHTML = headItem.innerHTML;
+                        // console.log("h5 new")
                     }
+                    thirdCategory.innerHTML = headItem.innerText;
+                    thirdCategory.setAttribute("data-category-title", convertToSlug(headItem.innerText));
+                    headItem.setAttribute("data-title", convertToSlug(headItem.innerText));
+
                 }
-                else if(allHeads.includes(headItem) && headItem.tagName == 'H6'){
+
+                else if (allHeads.includes(headItem) && headItem.tagName == 'H6') {
+
                     let fourthCategory = null;
-                    if(ddMenuHead.innerText != "Subcategory"){
+
+                    if (ddMenuHead.innerHTML != "Subcategory") {
                         fourthCategory = ddItem.cloneNode(true);
-                        fourthCategory.innerHTML = headItem.innerHTML;
                         ddMenu.appendChild(fourthCategory)
+                        // console.log("h6")
                     }
-                    else{
+
+                    else {
                         fourthCategory = ddMenuHead
-                        fourthCategory.innerHTML = headItem.innerHTML;
+                        // console.log("h6 new")
                     }
-                    
+
+                    fourthCategory.innerHTML = headItem.innerText;
+                    fourthCategory.setAttribute("data-category-title", convertToSlug(headItem.innerText));
+                    headItem.setAttribute("data-title", convertToSlug(headItem.innerText));
+
                 }
+
                 headItem = headItem.nextElementSibling;
             }
+
             ddItem.remove()
+            if (ddMenu.childNodes.length <= 0 && ddMenuHead.innerText == 'Subcategory') {
+                dropdown.remove()
+            }
+
             ddContainer.appendChild(clonedWrapper)
         }
+
+        else {
+            ddWrapper.remove()
+        }
+
     });
+
     ddWrapper.remove()
+    addScrollListener();
+    checkHaveDropdown();
+}
+function addScrollListener() {
+if(ddContainer != undefined){
+    let allHeadsCategory = ddContainer.querySelectorAll("[data-category-title]");
+    if(allHeadsCategory.length > 0){
+        allHeadsCategory.forEach(head =>{
+            head.addEventListener("click",(e) => {
+                let headSlug = e.target.getAttribute("data-category-title");
+                if(headSlug.length > 0){
+                    let elmTillScroll = richText.querySelector(`[data-title='${headSlug}']`);
+                    elmTillScroll.scrollIntoView({
+                        behavior:"smooth",
+                        block:"start",
+                        
+                    })
+                }
+            })
+        })
+    }
+}
+
+}
+function convertToSlug(Text) {
+    return Text.toLowerCase()
+        .replace(/[^\w ]+/g, "")
+        .replace(/ +/g, "-");
+}
+
+function checkHaveDropdown(){
+    let leftCol = document.querySelector(".left-col");
+    if(leftCol != undefined){
+        if(leftCol.childElementCount <= 0){
+            leftCol.style.marginRight = 0;
+        }
+        else{
+            return;
+        }
+    }
 }
 addCategories()
